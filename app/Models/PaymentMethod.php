9d8072ddfa6f4f77b05a84fa8dd22d310fdddda4
edu <89234306+edu455@users.noreply.github.com>
2024-09-30 14:45:55 -0500

    Create PaymentMethod.php model and migration

---
+++ app/Models/PaymentMethod.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class PaymentMethod extends Model
+{
+    public function user()
+    : BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    protected function casts()
+    : array
+    {
+        return [
+            'details' => 'array',
+        ];
+    }
+}
