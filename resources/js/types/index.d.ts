9d07e92dbacc2722a70787efa3348b2ef38b9a13
edu <89234306+edu455@users.noreply.github.com>
2024-09-30 15:28:09 -0500

    Change User type

--- resources/js/types/index.d.ts
+++ resources/js/types/index.d.ts
@@ -3,6 +3,10 @@ export interface User {
     name: string;
     email: string;
     email_verified_at?: string;
+    lastname: string | null;
+    type: string;
+    created_at: string;
+    updated_at: string;
 }
 
 export type PageProps<
