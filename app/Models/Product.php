3ca6f6bb8e1e0b252fd0507e2841f6f5fc0cd434
edu <89234306+edu455@users.noreply.github.com>
2024-10-01 04:27:07 -0500

    fix

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -5,10 +5,12 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Spatie\MediaLibrary\HasMedia;
+use Spatie\MediaLibrary\InteractsWithMedia;
 
-class Product extends Model
+class Product extends Model implements HasMedia
 {
-    use HasFactory;
+    use HasFactory, InteractsWithMedia;
     protected $fillable = ['name', 'description', 'price', 'category_id'];
 
     public function category()
