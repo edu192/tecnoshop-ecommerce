39c1d2b67cf46fdfd31af12b3ec776b0450590ab
edu <89234306+edu455@users.noreply.github.com>
2024-10-01 03:00:51 -0500

    Add Category.php model fillable array, products relationship and media collection

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -3,7 +3,22 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Category extends Model
 {
+    protected $fillable = ['name', 'description'];
+
+    public function products()
+    : HasMany
+    {
+        return $this->hasMany(Product::class);
+    }
+
+    public function registerMediaCollections()
+    : void
+    {
+        $this->addMediaCollection('main_image')->useFallbackPath(public_path('/images/no_image_placeholder.jpg'))->singleFile();
+        $this->addMediaCollection('background_image')->useFallbackPath(public_path('/images/no_image_placeholder.jpg'))->singleFile();
+    }
 }
