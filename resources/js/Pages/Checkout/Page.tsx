fdb5b3ee4935e56144561faa75f32fa004b966e7
edu <89234306+edu455@users.noreply.github.com>
2024-11-05 13:43:14 -0500

    Remove paypal option

--- resources/js/Pages/Checkout/Page.tsx
+++ resources/js/Pages/Checkout/Page.tsx
@@ -135,10 +135,6 @@ export default function CheckoutPage() {
                                         <RadioGroupItem value="credit_card" id="credit_card"/>
                                         <Label htmlFor="credit_card">Tarjeta de Crédito</Label>
                                     </div>
-                                    <div className="flex items-center space-x-2">
-                                        <RadioGroupItem value="paypal" id="paypal"/>
-                                        <Label htmlFor="paypal">PayPal</Label>
-                                    </div>
                                 </RadioGroup>
                                 {paymentMethod === 'credit_card' && (
                                     <div className="mt-4 space-y-4">
