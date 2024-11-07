d47611974efb2be819b86295f133e4e0450f0c88
edu <89234306+edu455@users.noreply.github.com>
2024-11-07 17:08:45 -0500

    Add sonner package

--- resources/js/app.tsx
+++ resources/js/app.tsx
@@ -1,9 +1,10 @@
 import '../css/app.css';
 import './bootstrap';
 
-import { createInertiaApp } from '@inertiajs/react';
-import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
-import { createRoot } from 'react-dom/client';
+import {createInertiaApp} from '@inertiajs/react';
+import {resolvePageComponent} from 'laravel-vite-plugin/inertia-helpers';
+import {createRoot} from 'react-dom/client';
+import {Toaster} from "sonner";
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
@@ -14,10 +15,15 @@ createInertiaApp({
             `./Pages/${name}.tsx`,
             import.meta.glob('./Pages/**/*.tsx'),
         ),
-    setup({ el, App, props }) {
+    setup({el, App, props}) {
         const root = createRoot(el);
 
-        root.render(<App {...props} />);
+        root.render(
+            <>
+                <Toaster richColors closeButton />
+                <App {...props} />
+            </>
+        );
     },
     progress: {
         color: '#4B5563',
