59989fd4a756364ff130cab9dbe8b77cde0f0c4f
edu <89234306+edu455@users.noreply.github.com>
2024-11-21 05:44:11 -0500

    Create Department.php model, migration and seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -21,6 +21,8 @@ class DatabaseSeeder extends Seeder
     public function run()
     : void
     {
+        // Poblar la tabla departamentos
+        $this->call(DepartmentSeeder::class);
         //Poblar la tabla usuarios con un usuario admin
         User::firstOrCreate([
             'name' => 'admin',
---
+++ database/seeders/DepartmentSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class DepartmentSeeder extends Seeder
+{
+    public function run()
+    : void
+    {
+        $departments = [
+            'Amazonas', 'Áncash', 'Apurímac', 'Arequipa', 'Ayacucho', 'Cajamarca', 'Callao', 'Cusco', 'Huancavelica',
+            'Huánuco', 'Ica', 'Junín', 'La Libertad', 'Lambayeque', 'Lima', 'Loreto', 'Madre de Dios', 'Moquegua',
+            'Pasco', 'Piura', 'Puno', 'San Martín', 'Tacna', 'Tumbes', 'Ucayali'
+        ];
+
+        foreach ($departments as $department) {
+            DB::table('departments')->insert([
+                'name' => $department,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ]);
+        }
+    }
+}
