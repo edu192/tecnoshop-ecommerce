524afca43197dca137783cbbf2f74e4292464c03
edu <89234306+edu455@users.noreply.github.com>
2024-11-22 22:59:46 -0500

    Add fillable array and relationship

--- app/Models/Department.php
+++ app/Models/Department.php
@@ -3,7 +3,15 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Department extends Model
 {
+    protected $fillable = ['name'];
+
+    public function orders()
+    : HasMany
+    {
+        return $this->hasMany(Order::class);
+    }
 }
