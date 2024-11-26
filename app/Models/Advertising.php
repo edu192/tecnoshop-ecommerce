9592d7ec19db9a956b06fbbf6f14b525daee5994
edu <89234306+edu455@users.noreply.github.com>
2024-11-26 07:13:00 -0500

    Advertising: add has many users relationship in Advertising.php model

--- app/Models/Advertising.php
+++ app/Models/Advertising.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Advertising extends Model
 {
@@ -11,5 +12,11 @@ class Advertising extends Model
     : BelongsTo
     {
         return $this->belongsTo(Product::class);
+    }
+
+    public function users()
+    : HasMany
+    {
+        return $this->hasMany(User::class, 'advertising_user','advertising_id');
     }
 }
