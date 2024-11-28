f7418bbac48cb53a82a02fd4cb803cd31b968e83
edu <89234306+edu455@users.noreply.github.com>
2024-11-28 15:34:50 -0500

    Add search filter banner page

--- resources/js/Pages/Backend/Banner/Index/Page.tsx
+++ resources/js/Pages/Backend/Banner/Index/Page.tsx
@@ -16,12 +16,14 @@ import CreateModal from "@/Pages/Backend/Banner/Partials/CreateModal";
 import UpdateModal from "@/Pages/Backend/Banner/Partials/UpdateModal";
 import DeleteModal from "@/Pages/Backend/Banner/Partials/DeleteModal";
 import BannerData = App.Data.BannerData;
+import {router} from "@inertiajs/react";
 
 type PageProps = {
     paginated_collection: PaginatedModelData<BannerData>
 };
 
 const Page = ({paginated_collection: {paginated_data, meta, links}}: PageProps) => {
+    const [searchValue, setSearchValue] = useState('')
     const [updateModalState, setUpdateModalState] = useState<{
         isOpen: boolean,
         banner: BannerData | null,
@@ -36,12 +38,16 @@ const Page = ({paginated_collection: {paginated_data, meta, links}}: PageProps) 
         isOpen: false,
         banner: null
     })
+    const handleSearch = () => {
+        router.visit(route('mantenimiento.banner.index', {'filter[name]': searchValue}));
+    }
     return (
         <BackendLayout pageName='Banners'>
             <div className="p-6 bg-white rounded-lg shadow">
                 <div className='flex justify-start pb-6 '>
-                    <Input placeholder='Buscar por id de banner' className='w-1/6'/>
-                    <Button>Buscar</Button>
+                    <Input placeholder='Buscar por nombre' className='w-1/6' value={searchValue}
+                           onChange={e => setSearchValue(e.target.value)}/>
+                    <Button onClick={handleSearch}>Buscar</Button>
                 </div>
                 <div className='flex justify-end'>
                     <CreateModal/>
@@ -50,7 +56,7 @@ const Page = ({paginated_collection: {paginated_data, meta, links}}: PageProps) 
                 </div>
                 <div>
                     <Table>
-                        <TableCaption>Lista de todas las entradas de productos del sistema.</TableCaption>
+                        <TableCaption>Lista de todos los banners del sistema.</TableCaption>
                         <TableHeader>
                             <TableRow>
                                 <TableHead className="w-[100px]">ID</TableHead>
